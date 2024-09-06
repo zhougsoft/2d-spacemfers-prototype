e8f345c6151577d7ca8f7f7961aec464925ea516
zhoug <90252209+zhougsoft@users.noreply.github.com>
2024-09-06 19:42:17 -0300

    include player state setup and teardown in create and delete queries

--- sql/players/create-player.sql
+++ sql/players/create-player.sql
@@ -1,9 +1,26 @@
--- create a new player and initialize their active ship as NULL
+-- create a new player and initialize their state records
+
 WITH new_player AS (
+    -- create a new player & return the id
     INSERT INTO players DEFAULT VALUES
     RETURNING player_id
 )
+
+-- initialize player active ship state record
 INSERT INTO player_active_ship (player_id, player_ship_id)
 SELECT player_id, NULL
 FROM new_player
-RETURNING player_id;
+ON CONFLICT (player_id) DO NOTHING;
+
+-- initialize player location state record
+INSERT INTO player_location (player_id, system_id, station_id, arrival_time, last_docked)
+SELECT player_id, 
+       1,  -- default starting system id
+       NULL,  -- replace with station id to have a default station
+       NOW() as arrival_time,
+       NULL as last_docked
+FROM new_player
+ON CONFLICT (player_id) DO NOTHING;
+
+-- return newly created player id
+SELECT player_id FROM new_player;
--- sql/players/delete-player.sql
+++ sql/players/delete-player.sql
@@ -1,8 +1,19 @@
--- delete a player and their associated records
+-- delete a player and their associated state records
+
 WITH deleted_player AS (
-    DELETE FROM player_active_ship
+    -- delete the player and return the id
+    DELETE FROM players
     WHERE player_id = 1
     RETURNING player_id
 )
-DELETE FROM players
+
+-- delete player active ship state record
+DELETE FROM player_active_ship
 WHERE player_id = (SELECT player_id FROM deleted_player);
+
+-- delete player location state record
+DELETE FROM player_location
+WHERE player_id = (SELECT player_id FROM deleted_player);
+
+-- return deleted player id
+SELECT player_id FROM deleted_player;
