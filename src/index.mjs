da6340c60390f6cb87aa6018d9068b1b4aed33cd
zhoug <90252209+zhougsoft@users.noreply.github.com>
2024-09-07 12:03:04 -0300

    driving postgres with node

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,10 @@
-import pg from 'pg'
+import dotenv from 'dotenv'
 import fs from 'fs/promises'
-import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
+import pg from 'pg'
+import { fileURLToPath } from 'url'
+
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -9,29 +12,42 @@ const __dirname = dirname(__filename)
 const { Client } = pg
 
 const client = new Client({
-  user: 'your_username',
+  user: process.env.POSTGRES_USER,
   host: 'localhost',
-  database: 'your_database',
-  password: 'your_password',
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PW,
   port: 5432,
 })
 
-async function runQuery(filename, params = []) {
+const runQuery = async (filename, params = []) => {
   try {
-    await client.connect()
-
     const filePath = join(__dirname, 'sql', filename)
     const query = await fs.readFile(filePath, 'utf-8')
-
     const result = await client.query(query, params)
-    console.log('Query result:', result.rows)
-  } catch (err) {
-    console.error('Error executing query:', err)
-  } finally {
-    await client.end()
+    console.log(`query ${filename} result:`, result.rows)
+    return result
+  } catch (error) {
+    console.error(`error executing query ${filename}:`, error)
   }
 }
 
-// Example usage:
-// runQuery('players/create-player.sql');
-// runQuery('player-state/set-active-ship.sql', [1, 1]);
+async function main() {
+  try {
+    await client.connect()
+    console.log('connected to db')
+
+    // --- RUN SQL QUERY FILES HERE -------------------------------------------
+    await runQuery('db-up.sql')
+    await runQuery('players/create-player.sql')
+    // await runQuery('player-state/set-active-ship.sql', [1, 1])
+    // await runQuery('db-down.sql')
+    // ------------------------------------------------------------------------
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await client.end()
+    console.log('disconnected from db')
+  }
+}
+
+main().catch(console.error)
