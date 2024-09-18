ce698599afe0c2c209b594326606af8cb642e1df
zhoug <90252209+zhougsoft@users.noreply.github.com>
2024-09-17 21:59:51 -0300

    basic client & express server communication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,81 @@
+const API_URL = 'http://localhost:6969'
+
+const Section = ({
+  children,
+  flex,
+}: {
+  children?: React.ReactNode | React.ReactNode[]
+  flex?: boolean
+}) => (
+  <div
+    style={{
+      display: flex ? 'flex' : 'block',
+      justifyContent: flex ? 'space-evenly' : 'initial',
+      border: '2px dotted black',
+      marginBottom: '1rem',
+      padding: '1rem',
+    }}>
+    {children}
+  </div>
+)
+
+const request = async (url: string, method?: string) => {
+  try {
+    const res = await fetch(url, {
+      method: method ?? 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 const App = () => {
+  const onDbUp = async () => {
+    const data = await request(`${API_URL}/api/db-up`)
+    console.log(data)
+  }
+
+  const onDbDown = async () => {
+    const data = await request(`${API_URL}/api/db-down`)
+    console.log(data)
+  }
+
+  const onCreatePlayer = async () => {
+    const data = await request(`${API_URL}/api/players/create`, 'POST')
+    console.log(data)
+  }
+
   return (
     <main>
       <h1>spacemfers</h1>
+      <Section flex>
+        <div>
+          <button onClick={onDbUp}>db up</button>
+          <pre>[output here]</pre>
+        </div>
+        <div>
+          <button onClick={onDbDown}>db down</button>
+          <pre>[output here]</pre>
+        </div>
+      </Section>
+      <Section>
+        <button onClick={onCreatePlayer}>create player</button>
+        <pre>[output here]</pre>
+      </Section>
+      <Section>
+        <button>get all players</button>
+        <pre>[output here]</pre>
+      </Section>
+      <Section>
+        <button>get player</button>
+        <input type="number" min="1" placeholder="player_id" />
+        <pre>[output here]</pre>
+      </Section>
     </main>
   )
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,79 @@
-console.log(
-  'TODO: make an express app & wrap some functionality in API endpoints'
-)
+import cors from 'cors'
+import express from 'express'
+import { client, runQuery } from './db'
+import { createPlayer } from './lib/players'
+import { createSolarSystem } from './utils'
+
+const PORT = 6969
+
+const main = async () => {
+  await client.connect()
+  console.log('connected to database...')
+
+  const app = express()
+  app.use(cors())
+
+  // --- db admin -------------------------------------------------------------
+  app.get('/api/db-up', async (req, res) => {
+    try {
+      await runQuery('db-down.sql')
+      await runQuery('db-up.sql')
+      const solarSystem = await createSolarSystem()
+
+      console.log('reset database')
+      res.json({ solarSystem })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  })
+
+  app.get('/api/db-down', async (req, res) => {
+    try {
+      await runQuery('db-down.sql')
+
+      console.log('dropped tables')
+      res.json({ msg: 'success' })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  })
+
+  // --- player routes --------------------------------------------------------
+  app.post('/api/players/create', async (req, res) => {
+    try {
+      const playerId = await createPlayer()
+      if (!playerId) throw Error('error creating player')
+
+      console.log('created player - id:', playerId)
+      res.json({ playerId })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  })
+
+  // TODO
+  app.get('/api/players', async (req, res) => {
+    res.json({ msg: 'ok' })
+  })
+
+  // TODO
+  app.get('/api/players/:id', async (req, res) => {
+    res.json({ msg: req.params.id })
+  })
+
+  // --- run server -----------------------------------------------------------
+  app.listen(PORT, () => {
+    console.log(`\n📡 spacemfers server running on port ${PORT}\n`)
+  })
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
+
+process.on('SIGINT', async () => {
+  console.log('\nshutting down spacemfers server...')
+  await client.end()
+  process.exit(0)
+})
