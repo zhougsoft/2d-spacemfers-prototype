bc4669783ccbb0b8ebc6117d6e89fbdabaa4c475
zhoug <90252209+zhougsoft@users.noreply.github.com>
2024-10-06 17:00:39 -0300

    check valid station using cte for adding player ships

--- src/sql/player-state/add-player-ship.sql
+++ src/sql/player-state/add-player-ship.sql
@@ -1,3 +1,9 @@
+WITH valid_station AS (
+    SELECT 1 
+    FROM station_info 
+    WHERE celestial_id = $3
+)
 INSERT INTO player_ships (player_id, ship_type_id, station_celestial_id)
-VALUES ($1, $2, $3)
+SELECT $1, $2, $3
+FROM valid_station
 RETURNING player_ship_id;
--- src/sql/player-state/remove-player-ship.sql
+++ src/sql/player-state/remove-player-ship.sql
@@ -1,9 +1,9 @@
-WITH deleted_ship AS (
+WITH removed_ship AS (
   DELETE FROM player_ships
   WHERE player_ship_id = $1
   RETURNING player_id, player_ship_id
 )
 UPDATE player_active_ship
 SET player_ship_id = NULL
-WHERE player_id = (SELECT player_id FROM deleted_ship)
-  AND player_ship_id = (SELECT player_ship_id FROM deleted_ship);
+WHERE player_id = (SELECT player_id FROM removed_ship)
+AND player_ship_id = (SELECT player_ship_id FROM removed_ship);
